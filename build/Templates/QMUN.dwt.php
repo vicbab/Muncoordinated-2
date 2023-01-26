de46e09be42942d1946209b29fc3ac9234f0d4bb
Victor B <vicbab@yandex.com>
2023-01-26 11:11:19 -0500

    add build

---
+++ build/Templates/QMUN.dwt.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>Document sans titre</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+</body>
+</html>
